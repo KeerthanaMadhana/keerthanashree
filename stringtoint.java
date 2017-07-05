4935c9280bb0e632d009857b4b7dc7684fdf9524
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-07-05 09:52:49 +0530

    Create stringtoint.java

---
+++ stringtoint.java
@@ -0,0 +1,9 @@
+package kshree;
+
+public class Stringtoint {
+	public static void main(String args[]){  
+		String s="200";  
+		int i=Integer.parseInt(s);  
+		System.out.println(s+100);
+		System.out.println(i+100);
+		}}  
