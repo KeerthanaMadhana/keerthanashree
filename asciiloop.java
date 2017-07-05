510824809661cffa4113684398a035b4aeea7cd3
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-07-05 12:34:48 +0530

    Create asciiloop.java

---
+++ asciiloop.java
@@ -0,0 +1,13 @@
+package kshree;
+
+public class Asciiloop {
+	 public static void main (String[]args) {
+
+		   for (int c=32; c<123; c++) {
+
+		    System.out.print((char)c);
+		   }
+		 }
+
+		}
+
