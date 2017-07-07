cfbc04b8d1a665ef7598d8dfb4ebe140554a50ba
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-07-07 10:01:38 +0530

    Create character.java

---
+++ character.java
@@ -0,0 +1,13 @@
+package ksh;
+import java.util.*;
+public class Characters {
+	public static void main(String[] args) {
+		
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the string:");
+String a=s.next();
+int n=a.length();
+System.out.println(n);
+	}
+}
+
